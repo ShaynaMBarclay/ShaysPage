7c73b5e7201a9ca3ccdc51dd17102ac95ebe57fb
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-08-18 18:34:30 -0400

    fixed welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import './Styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import MainScreen from './MainPage';
 import MediaPage from './MediaPage';
+import WelcomeScreen from './WelcomeScreen';
 
 function App() {
+  const [entered, setEntered] = useState(false);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainScreen />} />
-        <Route path="/media" element={<MediaPage />} />
-      </Routes>
-    </Router>
+    <>
+      {!entered ? (
+        <WelcomeScreen onEnter={() => setEntered(true)} />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainScreen />} />
+            <Route path="/media" element={<MediaPage />} />
+          </Routes>
+        </Router>
+      )}
+    </>
   );
 }
 
