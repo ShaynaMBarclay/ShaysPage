5216392db47612352b3204492b0caa01af6dba21
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-09-14 09:53:48 -0400

    media changed

--- src/MediaPage.jsx
+++ src/MediaPage.jsx
@@ -58,17 +58,8 @@ function MediaPage() {
           </div>
 
           <div className="media-gallery">
-            <div className="media-item"><img src="/media/wow1.png" alt="WoW 1" /></div>
-            <div className="media-item"><img src="/media/wow2.jpeg" alt="WoW 2" /></div>
-            <div className="media-item"><img src="/media/wow3.jpeg" alt="WoW 3" /></div>
-            <div className="media-item"><img src="/media/wow4.jpeg" alt="WoW 4" /></div>
+            <div className="media-item"><img src="/media/wow12.png" alt="WoW 12" /></div>
             <div className="media-item"><img src="/media/wow5.jpeg" alt="WoW 5" /></div>
-            <div className="media-item"><img src="/media/wow6.png" alt="WoW 6" /></div>
-            <div className="media-item"><img src="/media/wow7.png" alt="WoW 7" /></div>
-            <div className="media-item"><img src="/media/wow8.png" alt="WoW 8" /></div>
-            <div className="media-item"><img src="/media/wow9.png" alt="WoW 9" /></div>
-            <div className="media-item"><img src="/media/wow10.jpeg" alt="WoW 10" /></div>
-            <div className="media-item"><img src="/media/wow11.png" alt="WoW 11" /></div>
             <div className="media-item"><img src="/media/wowsept.jpeg" alt="WoW 12" /></div>
             <div className="media-item"><img src="/media/wowsept2.jpeg" alt="WoW 13" /></div>
             <div className="media-item"><img src="/media/wowsept3.jpeg" alt="WoW 14" /></div>
@@ -80,10 +71,6 @@ function MediaPage() {
           <h2>Final Fantasy XIV</h2>
           <div className="media-gallery">
             <div className="media-item"><img src="/media/ff1.jpeg" alt="FFXIV 1" /></div>
-            <div className="media-item"><img src="/media/ff2.png" alt="FFXIV 2" /></div>
-            <div className="media-item"><img src="/media/ff3.jpeg" alt="FFXIV 3" /></div>
-            <div className="media-item"><img src="/media/ff4.png" alt="FFXIV 4" /></div>
-            <div className="media-item"><img src="/media/ff5.jpeg" alt="FFXIV 5" /></div>
           </div>
         </div>
 
