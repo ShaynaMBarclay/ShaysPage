11d3314d3a29aa74dde8c254fc4eacc60b0ce882
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-10-21 16:48:41 -0400

    closed messaging

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -97,22 +97,24 @@ I'm a tech girly too ๋࣭ ⭑✮💻₊ ⊹ `;
       return;
     }
 
-    fetch("https://formspree.io/f/mldlbeda", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email, message }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          alert("Message sent! Thank you! 💗");
-          setName("");
-          setEmail("");
-          setMessage("");
-        } else {
-          alert("Oops! Something went wrong. Please try again.");
-        }
-      })
-      .catch(() => alert("Oops! Something went wrong."));
+    alert("Messaging is temporarily closed 💌 Please check back soon! And to the one harassing me, may you find peace (and maybe a nice patch of grass) 💕");
+
+    //fetch("https://formspree.io/f/mldlbeda", {
+      //method: "POST",
+     // headers: { "Content-Type": "application/json" },
+      //body: JSON.stringify({ name, email, message }),
+    //})
+      //.then((response) => {
+       // if (response.ok) {
+       //   alert("Message sent! Thank you! 💗");
+        //  setName("");
+         // setEmail("");
+       //   setMessage("");
+       // } else {
+       //   alert("Oops! Something went wrong. Please try again.");
+      //  }
+     // })
+     // .catch(() => alert("Oops! Something went wrong."));
   };
 
   return (
