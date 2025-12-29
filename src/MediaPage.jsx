402b69e06e868158fc463dd5ddb1b0bde6619d8c
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-12-29 11:43:55 -0500

    new media

--- src/MediaPage.jsx
+++ src/MediaPage.jsx
@@ -46,21 +46,10 @@ function MediaPage() {
         <div className="media-section">
           <h2>World of Warcraft</h2>
 
-          <div className="media-links">
-            <h3>Links to Wallpapers</h3>
-            <ul>
-              <li><a href="https://x.com/Sylvariae/status/1962197356339720661" target="_blank" rel="noopener noreferrer">Hallowfall Wallpapers</a></li>
-              <li><a href="https://x.com/Sylvariae/status/1959011669616857159" target="_blank" rel="noopener noreferrer">Suramar Wallpapers</a></li>
-              <li><a href="https://x.com/Sylvariae/status/1960710691272147070" target="_blank" rel="noopener noreferrer">Revendreth Wallpapers</a></li>
-              <li><a href="https://x.com/Sylvariae/status/1960379034644156870" target="_blank" rel="noopener noreferrer">Emerald Dream Wallpapers</a></li>
-              <li><a href="https://x.com/Sylvariae/status/1959693947460309031" target="_blank" rel="noopener noreferrer">Ardenweald Wallpapers</a></li>
-              <li><a href="https://x.com/Sylvariae/status/1964413159877595195" target="_blank" rel="noopener noreferrer">Eternal Palace Wallpapers</a></li>
-            </ul>
-          </div>
-
           <div className="media-gallery">
-            <div className="media-item"><img src="/media/wow12.png" alt="WoW 12.5" /></div>
-            <div className="media-item"><img src="/media/wow13.jpg" alt="WoW 5" /></div>
+            <div className="media-item"><img src="/media/wowpic1.png" alt="WoW 12.5" /></div>
+            <div className="media-item"><img src="/media/wowpic2.png" alt="WoW 5" /></div>
+            <div className="media-item"><img src="/media/wowpic4.png" alt="WoW 8" /></div>
             <div className="media-item"><img src="/media/wow5.jpeg" alt="WoW 5" /></div>
             <div className="media-item"><img src="/media/wowsept.jpeg" alt="WoW 12" /></div>
             <div className="media-item"><img src="/media/wowsept2.jpeg" alt="WoW 13" /></div>
@@ -74,6 +63,9 @@ function MediaPage() {
           <h2>Final Fantasy XIV</h2>
           <div className="media-gallery">
             <div className="media-item"><img src="/media/ff1.jpeg" alt="FFXIV 1" /></div>
+            <div className="media-item"><img src="/media/ffxiv2.png" alt="FFXIV 2" /></div>
+            <div className="media-item"><img src="/media/ffxiv3.png" alt="FFXIV 3" /></div>
+            <div className="media-item"><img src="/media/ffxiv5.png" alt="FFXIV 5" /></div>
           </div>
         </div>
 
@@ -82,6 +74,7 @@ function MediaPage() {
           <h2>Baldur's Gate 3</h2>
           <div className="media-gallery">
             <div className="media-item"><img src="/media/bg1.jpeg" alt="BG3 1" /></div>
+            <div className="media-item"><img src="/media/bg3two.png" alt="BG3 3" /></div>
             <div className="media-item"><img src="/media/bg2.jpeg" alt="BG3 2" /></div>
             <div className="media-item"><img src="/media/bg3.jpeg" alt="BG3 3" /></div>
           </div>
